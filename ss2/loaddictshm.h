12b4e7e1c67005fd6e11d240e9a5fd9b29c025d7
cheonguk <40656043+cheonguk@users.noreply.github.com>
2019-01-11 13:00:22 +0900

    Update loaddictshm.h
    ok

--- ss2/loaddictshm.h
+++ ss2/loaddictshm.h
@@ -18,12 +18,5 @@ long CreateDictShm(long lCount, char** pResult, char* pShm);
 long ReadCntSetBatch(char* pDictShm,
                           long  lDictCount,
                           char* shs_key,
-<<<<<<< HEAD
-                          char* sval,
-                          char* sprompttt);
-// add commont
-=======
                           char* sprompt);
-
->>>>>>> origin/master
 #endif
